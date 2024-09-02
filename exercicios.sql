d26ba74437f23e69b9478f651dccb980278f9a9f
Thiago <thiagomarzari1@gmail.com>
2024-09-02 10:46:12 -0300

    ex
    encontrar funcionarios admitidos nos ultimos 6 meses

--- exercicios.sql
+++ exercicios.sql
@@ -8,3 +8,16 @@ SELECT F.Pnome,
 			ELSE 'ALTO'
 		END AS 'Categoria_salario'
 FROM FUNCIONARIO AS F;
+
+
+--encontrar funcionarios admitidos nos ultimos 6 meses
+
+SELECT F.Pnome, 
+		F.Data_Admissao,
+		CASE
+			WHEN DATEDIFF(YEAR, f.Data_Admissao, GETDATE()) = 0 AND
+				DATEDIFF(MONTH, F.Data_Admissao, GETDATE()) <= 6
+				THEN 'Admitido'
+			ELSE 'NAO'
+		END AS 'admitido nos últimos 6 meses?'
+FROM FUNCIONARIO AS F
