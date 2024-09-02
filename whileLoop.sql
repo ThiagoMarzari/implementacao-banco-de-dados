3e2b320a9ec8dca95a17a87d0b85a7eb0ca45cb5
Thiago <thiagomarzari1@gmail.com>
2024-09-02 11:05:18 -0300

    WHILE

--- whileLoop.sql
+++ whileLoop.sql
@@ -1,8 +1,13 @@
 
 DECLARE @valor INT
 SET @valor = 0
-WHILE @valor<10
+--Pode ser um select aqui dentro se a condicao é verdadeira
+WHILE @valor<10 
 	BEGIN
+		SET @valor = @valor + 1;
+		IF @valor % 2 <> 0
+			CONTINUE;
 		PRINT 'Número: ' + CAST(@valor AS VARCHAR(2))
-		SET @valor = @valor+1
 	END
+
+-- <> == =
