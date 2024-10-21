92114e4ef2efc6182260b25449b9ebead2a4266b
ThiagoMarzari <87370358+ThiagoMarzari@users.noreply.github.com>
2024-10-21 08:30:45 -0300

    after | insteaf of

--- triggers/SQLQuery1.sql
+++ triggers/SQLQuery1.sql
@@ -1,12 +1,23 @@
 
+
+select * from FUNCIONARIO
+
+-- Primeiro ele insere e depois executa o que está dentro do trigger --
+
+CREATE TRIGGER olaMundo
+ON FUNCIONARIO
+AFTER INSERT -- Roda após o cõdigo que causou o trigger
+AS
+PRINT 'Olá mundo';
+
 INSERT FUNCIONARIO(Pnome, Unome, Cpf)
 VALUES
 ('Rodrigo', 'Rossato', '98765432121');
 
-select * from FUNCIONARIO
 
-CREATE TRIGGER olaMundo
+
+CREATE TRIGGER olaDenovo
 ON FUNCIONARIO
-AFTER INSERT
+INSTEAD OF INSERT -- substituiu a operação que causou o disparo
 AS
-PRINT 'Olá mundo';
+PRINT 'Olá de novo!'
