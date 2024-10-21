f8599d598ec782e4df30ec40da6507bb534eb43e
ThiagoMarzari <87370358+ThiagoMarzari@users.noreply.github.com>
2024-10-21 10:29:58 -0300

    TEXTO ADC

--- triggers/SQLQuery3.sql
+++ triggers/SQLQuery3.sql
@@ -7,6 +7,7 @@ SELECT * FROM FUNCIONARIO
 
 --Crie um trigger que não permita a inserção 
 --de um funcionário com o mesmo nome completo (Pnome, Minicial, Unome)
+--Esse tipo de operação não compensa fazer com o AFTER OF, pois o AFTER OF RODA DEPOIS DO EVENTO ENTÃO FICA MAIS DIFICIL DE CHECAR SE O FUNCIONOARIO JÁ EXISTE
 ALTER TRIGGER insert_funcionario
 ON FUNCIONARIO
 INSTEAD OF INSERT
@@ -28,34 +29,5 @@ BEGIN
         SELECT Pnome, Unome, Cpf
         FROM inserted; -- Pego os dados da pessoa que insere no insert acima
 		PRINT 'Funcionario adicionado com sucesso';
-	END
-END
-
-
---COM AFTER, FICA MAIS CHATO DE FAZER, NÃO COMPENSA 
-ALTER TRIGGER trg_after_insert_funcionario
-ON FUNCIONARIO
-AFTER INSERT
-AS
-BEGIN
-	DECLARE @Pnome VARCHAR(50),
-			@Unome VARCHAR(50);
-
-	SELECT @Pnome = I.Pnome , @Unome = I.Unome 
-	FROM inserted AS I;
-
-	IF EXISTS (
-		SELECT 1
-		FROM FUNCIONARIO
-		WHERE Pnome = @Pnome AND Unome = @Unome
-	)
-	BEGIN
-		PRINT 'Funcionário já existente!' + @Pnome;
-		ROLLBACK TRANSACTION;
-	END
-	ELSE
-	BEGIN
-		PRINT 'Funcionário adicionado!';
-		COMMIT TRANSACTION;
 	END
 END
