115d3f8ba36f5679c9566542819d158af1a47455
ThiagoMarzari <87370358+ThiagoMarzari@users.noreply.github.com>
2024-10-21 10:28:11 -0300

    up

--- triggers/SQLQuery3.sql
+++ triggers/SQLQuery3.sql
@@ -1,7 +1,7 @@
 
 INSERT FUNCIONARIO(Pnome, Unome, Cpf)
 VALUES
-('Juca', 'Rossato', '11755432121');
+('Gustavo', 'Rossato', '11715432121');
 
 SELECT * FROM FUNCIONARIO
 
@@ -26,7 +26,36 @@ BEGIN
 	BEGIN
 		INSERT INTO FUNCIONARIO (Pnome, Unome, Cpf)
         SELECT Pnome, Unome, Cpf
-        FROM inserted;
+        FROM inserted; -- Pego os dados da pessoa que insere no insert acima
 		PRINT 'Funcionario adicionado com sucesso';
+	END
+END
+
+
+--COM AFTER, FICA MAIS CHATO DE FAZER, NÃO COMPENSA 
+ALTER TRIGGER trg_after_insert_funcionario
+ON FUNCIONARIO
+AFTER INSERT
+AS
+BEGIN
+	DECLARE @Pnome VARCHAR(50),
+			@Unome VARCHAR(50);
+
+	SELECT @Pnome = I.Pnome , @Unome = I.Unome 
+	FROM inserted AS I;
+
+	IF EXISTS (
+		SELECT 1
+		FROM FUNCIONARIO
+		WHERE Pnome = @Pnome AND Unome = @Unome
+	)
+	BEGIN
+		PRINT 'Funcionário já existente!' + @Pnome;
+		ROLLBACK TRANSACTION;
+	END
+	ELSE
+	BEGIN
+		PRINT 'Funcionário adicionado!';
+		COMMIT TRANSACTION;
 	END
 END
