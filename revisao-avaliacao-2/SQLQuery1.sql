c75486eb3fa11b2c5dda052784eca0e0b1171b08
ThiagoMarzari <thiagomarzari1@gmail.com>
2024-10-22 18:40:45 -0300

    revisaozinha

---
+++ revisao-avaliacao-2/SQLQuery1.sql
@@ -0,0 +1,27 @@
+
+BEGIN TRANSACTION
+
+DECLARE @Error INT;
+
+INSERT INTO FUNCIONARIO(Pnome, Unome, Cpf)
+VALUES ('Thiago', 'Marzari', '45345245372')
+
+SET @Error = @@ERROR;
+
+INSERT INTO DEPARTAMENTO (Dnome, Dnumero, Cpf_gerente, Data_inicio_gerente)
+VALUES('Marketing', 1, '98765432400', '2023-09-29')
+
+SET @Error = @Error + @@ERROR;
+
+IF @@ERROR = 0
+BEGIN
+	ROLLBACK TRANSACTION
+	PRINT 'Erro detectado. Transação revertida.'
+END
+ELSE
+BEGIN
+	COMMIT TRANSACTION
+	PRINT 'Transação concluida.'
+END
+
+select * from FUNCIONARIO
