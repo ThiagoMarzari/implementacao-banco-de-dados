5c7c09651cdc1e474648493f7625de7cca7848cc
ThiagoMarzari <thiagomarzari1@gmail.com>
2024-10-22 19:14:43 -0300

    up

--- revisao-avaliacao-2/SQLQuery3.sql
+++ revisao-avaliacao-2/SQLQuery3.sql
@@ -2,3 +2,37 @@
 --Verifique o estoque antes de permitir a compra e,
 --se o estoque for insuficiente, reverta a transação. 
 --Use uma tabela chamada PRODUTO para registrar o estoque dos produtos.
+
+--CREATE TABLE PRODUTO (
+--    Id INT PRIMARY KEY,
+--    Nome VARCHAR(50),
+--    Quantidade INT
+--);
+
+--INSERT INTO PRODUTO (Id, Nome, Quantidade) VALUES (1, 'Notebook', 10), (2, 'Mouse', 50);
+
+BEGIN TRANSACTION
+
+DECLARE @produtoId INT = 2;
+DECLARE @quantidadeCompra INT = 5;
+
+--Verificar estoque
+IF (SELECT Quantidade FROM PRODUTO WHERE ID = @produtoId) >= @quantidadeCompra
+BEGIN
+
+	--Diminuindo o estoque do produto
+	UPDATE PRODUTO
+	SET Quantidade = Quantidade - @quantidadeCompra
+	WHERE Id = @produtoId
+
+	PRINT 'Compra realizada com sucesso';
+
+	COMMIT;
+END
+ELSE 
+BEGIN
+	PRINT 'Comprar fracassada!';
+	ROLLBACK;
+END
+
+select * from PRODUTO;
