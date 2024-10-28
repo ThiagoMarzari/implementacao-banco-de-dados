363b40c95136e325008fa50f8b74ac3c3c6268ab
ThiagoMarzari <87370358+ThiagoMarzari@users.noreply.github.com>
2024-10-28 09:06:05 -0300

    tabela de pessoas criada

--- aula - 28-10-24/SQLQuery1.sql
+++ aula - 28-10-24/SQLQuery1.sql
@@ -16,5 +16,9 @@ CREATE TABLE tbl_pessoa(
 	id INT PRIMARY KEY IDENTITY(100, 10),
 	nome_pessoa VARCHAR(50),
 	nome_pet VARCHAR(50),
-	num_pet INT 
-)
+	num_pet INT CHECK(num_pet > 0), -- Para cadastrar o num tem que ser > 0
+	idade INT CHECK (idade BETWEEN 18 AND 95),
+	sexo_pet CHAR CHECK (sexo_pet IN ('M', 'F', 'N'))
+);
+
+select * from tbl_pessoa
